3ac108105ad68f89ae9eb356a0228c48ffa700c9
Kylie Sutton <kyliesutton@my.unt.edu>
2025-12-04 18:46:04 -0600

    Update rateLimiter.js

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -1,17 +1,21 @@
-//max 3 login attempts per IP every 5 minutes
-import rateLimit from 'express-rate-limit';
-export const loginLimiter = rateLimit({
+const rateLimit = require('express-rate-limit');
+const loginLimiter = rateLimit({
   windowMs: 5 * 60 * 1000,
   max: 3,
-  message: "Too many login attempts. Please try again later",
+  message: "Too many login attempts. Please try again later.",
   standardHeaders: true,
   legacyHeaders: false,
 });
 
-export const apiLimiter = rateLimit({
+const apiLimiter = rateLimit({
   windowMs: 1 * 60 * 1000,
-  max: 50,
-  message: "Too many requests. Please slow down.",
+  max: 100,
+  message: "Too many requests",
   standardHeaders: true,
   legacyHeaders: false,
 });
+
+module.exports = {
+  loginLimiter,
+  apiLimiter,
+};
