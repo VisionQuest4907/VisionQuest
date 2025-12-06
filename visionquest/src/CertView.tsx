379fc428da1f08c701f6c6b6da43db4ff5dea2ed
Foxglove21 <brightflores@gmail.com>
2025-12-05 21:53:39 -0600

    More files for application for pages

--- visionquest/src/CertView.tsx
+++ visionquest/src/CertView.tsx
@@ -1,8 +1,8 @@
-import './dashboard.css';
+import './CertView.css';
 import {useNavigate, Link} from 'react-router-dom';
 import type {MouseEvent} from 'react';
 
-function Dashboard() {
+function CertView() {
     const goTo = useNavigate();
     //test function to get to dashboard until security funcs
     function toSetting(e: MouseEvent<HTMLAnchorElement>){
@@ -17,18 +17,18 @@ function Dashboard() {
         e.preventDefault();
         goTo("/dashboard");
     }
-    return  (
-        <div className="dash-pg">
-            <nav className="dash-nav">
+    return(
+        <div className="cert-pg">
+            <nav className="cert-nav">
                 <a href="/dashboard" onClick={toDash}>Dashboard</a>
                 <a href="#">Downloadables</a>
                 <a href="/certview" onClick={toCertView}>Certificates</a>
                 <a href="/settings" onClick={toSetting}>Settings</a>
                 <a href="#">Profile</a>
             </nav>
+            <h1>Certificate Viewer</h1>
         </div>
-    
     );
 }
 
-export default Dashboard;
+export default CertView;
--- visionquest/src/Setting.tsx
+++ visionquest/src/Setting.tsx
@@ -1,8 +1,8 @@
-import './dashboard.css';
+import './Setting.css'
 import {useNavigate, Link} from 'react-router-dom';
 import type {MouseEvent} from 'react';
 
-function Dashboard() {
+function Setting() {
     const goTo = useNavigate();
     //test function to get to dashboard until security funcs
     function toSetting(e: MouseEvent<HTMLAnchorElement>){
@@ -17,18 +17,18 @@ function Dashboard() {
         e.preventDefault();
         goTo("/dashboard");
     }
-    return  (
-        <div className="dash-pg">
-            <nav className="dash-nav">
+    return(
+        <div className="setting-pg">
+            <nav className="setting-nav">
                 <a href="/dashboard" onClick={toDash}>Dashboard</a>
                 <a href="#">Downloadables</a>
                 <a href="/certview" onClick={toCertView}>Certificates</a>
                 <a href="/settings" onClick={toSetting}>Settings</a>
                 <a href="#">Profile</a>
             </nav>
+            <h1>Settings</h1>
         </div>
-    
     );
 }
 
-export default Dashboard;
+export default Setting;
