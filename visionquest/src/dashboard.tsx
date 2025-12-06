1d8bc26da71777689d443d0f8389d2e10f5887e0
Foxglove21 <brightflores@gmail.com>
2025-12-05 21:51:44 -0600

    Update dashboard.tsx

--- visionquest/src/dashboard.tsx
+++ visionquest/src/dashboard.tsx
@@ -1,10 +1,34 @@
-import loginback from './assets/loginback.png'
-import './LoginPage.css'
+import './dashboard.css';
+import {useNavigate, Link} from 'react-router-dom';
+import type {MouseEvent} from 'react';
 
-function LoginPage() {
-  return  (
+function Dashboard() {
+    const goTo = useNavigate();
+    //test function to get to dashboard until security funcs
+    function toSetting(e: MouseEvent<HTMLAnchorElement>){
+        e.preventDefault();
+        goTo("/setting");
+    }
+    function toCertView(e:MouseEvent<HTMLAnchorElement>){
+        e.preventDefault();
+        goTo("/certview");
+    }
+    function toDash(e:MouseEvent<HTMLAnchorElement>){
+        e.preventDefault();
+        goTo("/dashboard");
+    }
+    return  (
+        <div className="dash-pg">
+            <nav className="dash-nav">
+                <a href="/dashboard" onClick={toDash}>Dashboard</a>
+                <a href="#">Downloadables</a>
+                <a href="/certview" onClick={toCertView}>Certificates</a>
+                <a href="/settings" onClick={toSetting}>Settings</a>
+                <a href="#">Profile</a>
+            </nav>
+        </div>
     
-  )
+    );
 }
 
-export default LoginPage
+export default Dashboard;
