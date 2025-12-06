ec4b21b88f6c33bc4ba34d4be66a579aeda7837e
Foxglove21 <brightflores@gmail.com>
2025-12-05 21:52:36 -0600

    Update main.tsx

--- visionquest/src/main.tsx
+++ visionquest/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import {BrowserRouter} from 'react-router-dom';
 import './index.css'
-import LoginPage from './LoginPage.tsx'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <LoginPage />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
