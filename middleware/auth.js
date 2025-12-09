0e937570e99dc282839d7986f2ef49f924747726
Kylie Sutton <kyliesutton@my.unt.edu>
2025-12-08 21:42:39 -0600

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/user');
 const { JWT_SECRET } = require('../config/env');
 
 const requireAuth = async (req, res, next) => {
