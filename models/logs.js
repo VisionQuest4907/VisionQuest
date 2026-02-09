e49b3c2c0568f4b4dabb30cad0d6c7927bb775a1
everj56 <sydn3yj909@gmail.com>
2026-02-09 12:16:55 -0600

    Add moduleID field to logs schema

--- models/logs.js
+++ models/logs.js
@@ -4,6 +4,7 @@ const logSchema = new mongoose.Schema({
     userRef: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     userID: { type: String, required: true },
     action: { type: String, required: true },
+    moduleID: { type: String },
     timestamp: { type: Date, default: Date.now },
     details: { type: mongoose.Schema.Types.Mixed },
 
