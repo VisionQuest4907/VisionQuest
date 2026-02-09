8f31a8b71b0bd66c5d23740db8e98ee757809de8
everj56 <sydn3yj909@gmail.com>
2026-02-09 10:46:24 -0600

    Fix import path for User model in auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const User = require('../models/users');
 const { JWT_SECRET } = require('../config/env');
 
 const requireAuth = async (req, res, next) => {
