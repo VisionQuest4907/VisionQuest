a0782af74e32596ac540ea4f493f454b8e7006c4
Foxglove21 <brightflores@gmail.com>
2026-02-19 13:43:35 -0600

    more routes

--- visionquest/src/App.tsx
+++ visionquest/src/App.tsx
@@ -13,12 +13,20 @@ import VideoFour from './VideoFour.tsx';
 import GameOne from './ModOneGame.tsx';
 import QuizOne from './QuizOne.tsx';
 import ModOneCert from './ModOneCert.tsx';
+import About from './About.tsx';
+import RegistrationPage from './register.tsx';
+import ExpectModTwo from './ModTwoExpect.tsx';
+import VideoOneModTwo from './ModTwoVidOne.tsx';
+import VideoTwoModTwo from './ModTwoVidTwo.tsx';
+import ModTwoGameOne from './ModTwoGameOne.tsx';
+import VideoThreeModTwo from './ModTwoVidThree.tsx';
 
 export default function App() {
   return (
     <Routes>
       <Route element={<PageMap />}>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<About />} />
+        <Route path="/loginpage" element={<LoginPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/setting" element={<Setting />} />
         <Route path="/certview" element={<CertView />} />
@@ -31,6 +39,12 @@ export default function App() {
         <Route path="/moduleone/game" element={<GameOne />} />
         <Route path="/moduleone/quiz" element={<QuizOne />} />
         <Route path="/moduleone/module-one-cert" element={<ModOneCert />} />
+        <Route path="/registration" element={<RegistrationPage />} />
+        <Route path="/moduletwo/expectations" element={<ExpectModTwo />} />
+        <Route path="/moduletwo/video-one" element={<VideoOneModTwo />} />
+        <Route path="/moduletwo/video-two" element={<VideoTwoModTwo />} />
+        <Route path="/moduletwo/game-one" element={<ModTwoGameOne />} />
+        <Route path="/moduletwo/video-three" element={<VideoThreeModTwo />} />
         <Route path="*" element={<div style={{ padding: '2rem' }}><h2>Page not found</h2></div>} />
       </Route>
     </Routes>
