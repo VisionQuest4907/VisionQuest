19a6abb1b75cd8a7335eb879be3c0d36fa07677c
Foxglove21 <brightflores@gmail.com>
2026-02-19 13:45:31 -0600

    Update LoginPage.tsx

--- visionquest/src/LoginPage.tsx
+++ visionquest/src/LoginPage.tsx
@@ -1,6 +1,6 @@
 import './LoginPage.css';
 import {useNavigate} from 'react-router-dom';
-import type {FormEvent} from 'react';
+import type {FormEvent, MouseEvent} from 'react';
 
 function LoginPage() {
   const goTo = useNavigate();
@@ -9,20 +9,34 @@ function LoginPage() {
     e.preventDefault();
     goTo("/dashboard");
   }
+  function toLanding(e:MouseEvent<HTMLAnchorElement>){
+    e.preventDefault();
+    goTo("/");
+  }
+  function toRegister(e:MouseEvent<HTMLAnchorElement>){
+    e.preventDefault();
+    goTo("/registration");
+  }
   return  (
     <div className="login-pg">
-      <h1>LOGIN</h1>
-      <div>
-        <form onSubmit={toDashTest}>
-	        <label htmlFor="uname">Username</label><br></br>
-	        <input type="text" id="uname" name="uname"></input><br></br>
-	        <label htmlFor="pwd">Password</label><br></br>
-	        <input type="password" id="pwd" name="pwd"></input><br></br>
-          <input type="submit" value="Submit"></input>
-        </form>
-      </div>
-      <div className="other">
-          
+      <div className="loginDeets">
+        <h1>LOGIN</h1>
+          <form onSubmit={toDashTest}>
+            <label htmlFor="uname">Username</label><br></br>
+            <input type="text" id="uname" name="uname"></input><br></br>
+            <label htmlFor="pwd">Password</label><br></br>
+            <input type="password" id="pwd" name="pwd"></input><br></br>
+            <input className="submitButton" type="submit" value="Login"></input>
+          </form>
+          <h3>Not registered?</h3>
+        <div className="other">
+          <button className="back-button">
+            <a href="/" onClick={toLanding}>Back</a>
+          </button>
+          <button className="sign-up-button">
+            <a href="/" onClick={toRegister}>Register</a>
+          </button>
+        </div>
       </div>
     </div>
   );
