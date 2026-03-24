28da341523795ef143f18839cee4de9f7108c2d2
everj56 <sydn3yj909@gmail.com>
2026-03-24 10:01:18 -0500

    Increase max login attempts in rate limiter
    increased to rate limit max to 5 so that account lockout will work after 3 attempts and rate limiter will fire after 5

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -1,7 +1,7 @@
 const rateLimit = require('express-rate-limit');
 const loginLimiter = rateLimit({
   windowMs: 5 * 60 * 1000,
-  max: 3,
+  max: 5, //increased to 5 so that account will be locked for 5 minutes after 3 attempts and rate limiter will fire after 5
   message: "Too many login attempts. Please try again later.",
   standardHeaders: true,
   legacyHeaders: false,
