05256bcbd543df39e553fcff33eecebebb2dc98c
everj56 <sydn3yj909@gmail.com>
2026-03-24 10:29:40 -0500

    Refactor env.js to use CommonJS syntax
    This is the current config/env.js that uses CommonJS which matches the rest of the backend. This is the current config/env.js deployed on AWS backend.

--- config/env.js
+++ config/env.js
@@ -1,21 +1,21 @@
-//use environmental variables for secrets to avoid hardcoding 
-
-import dotenv from 'dotenv';
+const dotenv = require("dotenv");
 dotenv.config();
 
-export const {
-  PORT = 5000,
-  MONGO_URI,
-  JWT_SECRET,
-  NODE_ENV = 'development',
-} = process.env;
+const PORT = process.env.PORT || 5000;
+
+const MONGO_URI = process.env.MONGO_URI || process.env.DB_URI;
+
+const JWT_SECRET = process.env.JWT_SECRET;
+const NODE_ENV = process.env.NODE_ENV || "development";
 
 if (!MONGO_URI) {
-  console.error('MONGO_URI not set in .env');
+  console.error("MONGO_URI / DB_URI not set in environment");
   process.exit(1);
 }
 
 if (!JWT_SECRET) {
-  console.error('JWT_SECRET not sete in .env');
+  console.error("JWT_SECRET not set in environment");
   process.exit(1);
 }
+
+module.exports = { PORT, MONGO_URI, JWT_SECRET, NODE_ENV }; //
