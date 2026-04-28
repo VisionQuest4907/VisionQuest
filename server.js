304e52390d78282c41ad30c036d09a8107f98fa8
Kylie Sutton <kyliesutton@my.unt.edu>
2026-04-27 19:45:54 -0500

    Update server.js
    add security.js

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const userRoutes = require('./routes/users');
 const moduleRoutes = require("./routes/moduleRoutes");
 const logRoutes = require("./routes/logRoutes");
 const userDataRoutes = require("./routes/userDataRoutes");
+const security = require("./middleware/security");
 
 const app = express();
 
@@ -34,6 +35,9 @@ app.disable('x-powered-by');
 app.use(helmet({
   crossOriginResourcePolicy: { policy: "same-site" },
 }));
+
+//Security middleware
+app.use(security)
 
 //safer body parsing with request size limited 
 app.use(express.json({ limit: '200kb' }));
