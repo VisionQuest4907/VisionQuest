6856dcc4791adea241ff480de54fa85d790834e4
malludaks <malludakshayanireddy@gmail.com>
2026-05-01 21:44:39 -0500

    Add files via upload

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   testEnvironment: 'node',
   setupFiles: ['<rootDir>/tests/setup.js'],
+  globalTeardown: '<rootDir>/tests/globalTeardown.js',
   testMatch: ['**/tests/**/*.test.js'],
   maxWorkers: 1,
 };
